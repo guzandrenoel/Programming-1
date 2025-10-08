29146f01b0e1cce334f0e2f6ee3a46709a5125d9
guzandrenoel <guzandrenoel@gmail.com>
2025-10-09 06:56:44 +0800

    Added Examples

---
+++ Examples/digit-counter.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    int i = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    for (; num != 0; i++) {
+        num /= 10;
+    }
+
+    printf("Number of digits: %d", i);
+    return 0;
+}
